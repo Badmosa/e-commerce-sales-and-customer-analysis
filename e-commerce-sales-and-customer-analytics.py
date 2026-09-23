4bd852e4cfc84662ac9e4bbeface1b37d3091631
Badmos Adesola <badmosayomide02@gmail.com>
2026-09-23 17:04:43 +0100

    Analyze e-commerce sales data

---
+++ e-commerce-sales-and-customer-analytics.py
@@ -0,0 +1,22 @@
+import kagglehub
+import pandas as pd
+
+path = kagglehub.dataset_download(
+    "datascikhan/e-commerce-sales-and-customer-analytics"
+)
+
+file_path = path + "/ecommerce_sales_customer_analytics_150k.csv"
+
+df = pd.read_csv(file_path)
+
+df.head()
+---------------------------
+# Data Inspection
+print(df.columns)
+
+
+# Inspecting the dataset info.
+print(df.info())
+
+# Inspecting for duplicates in the dataset.
+df.duplicated().sum()
